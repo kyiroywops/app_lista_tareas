92eabb559eaef3c8d1e369044448951310d78ab0
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-08-23 16:53:59 -0400

    state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './componentes/Header';
-import React from 'react';
+import React, {useState} from 'react';
+import FormularioTareas from './componentes/FormularioTareas';
 
 const App = () => {
+    const [tareas, setTareas] = useState()  
   return (
     <div className='contenedor'>
       <Header />
+      <FormularioTareas />
     </div>
   );
 }

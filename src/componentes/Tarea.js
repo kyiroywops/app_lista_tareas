f8e568aaec10bbdd91779a3753ea1df229023f58
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-08-24 19:22:01 -0400

    tarea js

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheckSquare} from "@fortawesome/free-solid-svg-icons";
 
 const Tarea = ({tarea}) => {
     return (
         <li className='lista-tareas__tarea'>
-            {tarea.texto}
+            <FontAwesomeIcon icon={faCheckSquare} className='lista-tareas__icono lista-tareas__icono-check' />
+            <div className='lista-tareas__texto'>
+                {tarea.texto}
+            </div>
+            <div className='lista-tareas__tarea'>
+                <FontAwesomeIcon icon={faCheckSquare} className='lista-tareas__icono lista-tareas__icono-check' />
+
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faCheckSquare} className='lista-tareas__icono lista-tareas__icono-check' />
+            </div>
         </li>
     );
 }

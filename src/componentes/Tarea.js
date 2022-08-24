56441dc7fd1bb3ad620cc95dd34f612846c5bbea
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-08-24 19:28:10 -0400

    fa iconos

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheckSquare} from "@fortawesome/free-solid-svg-icons";
+import {faCheckSquare, faEdit, faTimes} from "@fortawesome/free-solid-svg-icons";
 
 const Tarea = ({tarea}) => {
     return (
@@ -9,13 +9,12 @@ const Tarea = ({tarea}) => {
             <div className='lista-tareas__texto'>
                 {tarea.texto}
             </div>
-            <div className='lista-tareas__tarea'>
-                <FontAwesomeIcon icon={faCheckSquare} className='lista-tareas__icono lista-tareas__icono-check' />
+            <div className='lista-tareas__contenedor-botones'>
+                <FontAwesomeIcon icon={faEdit} className='lista-tareas__icono lista-tareas__icono-accion' />
+                <FontAwesomeIcon icon={faTimes} className='lista-tareas__icono lista-tareas__icono-accion' />
 
             </div>
-            <div>
-                <FontAwesomeIcon icon={faCheckSquare} className='lista-tareas__icono lista-tareas__icono-check' />
-            </div>
+    
         </li>
     );
 }

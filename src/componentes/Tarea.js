b77f3e19baecd553a7747e673ad15707fd9f87dc
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-08-25 23:12:55 -0400

    iconos check

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheckSquare, faEdit, faTimes} from "@fortawesome/free-solid-svg-icons";
+import {faCheckSquare, faEdit, faTimes, faSquare} from "@fortawesome/free-solid-svg-icons";
 
 const Tarea = ({tarea}) => {
 
@@ -14,7 +14,7 @@ const Tarea = ({tarea}) => {
 
     return (
         <li className='lista-tareas__tarea'>
-            <FontAwesomeIcon icon={faCheckSquare} className='lista-tareas__icono lista-tareas__icono-check' />
+            <FontAwesomeIcon icon={tarea.completada ? faCheckSquare : faSquare} className='lista-tareas__icono lista-tareas__icono-check' />
             <div className='lista-tareas__texto'>
                 {editandoTarea ?
                     <form action='' className='formulario-editar-tarea' onSubmit={handleSubmit}>
